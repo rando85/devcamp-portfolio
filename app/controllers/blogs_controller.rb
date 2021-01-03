0594bb086b2638eb7e0494845b7b28dc08cf536f
Daniel Cisneros Medina <webmasterofpuppetz@gmail.com>
2021-01-02 20:59:18 -0600

    changes to controller labels

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,7 +28,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @blog, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
@@ -56,7 +56,7 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was deleted.' }
       format.json { head :no_content }
     end
   end
