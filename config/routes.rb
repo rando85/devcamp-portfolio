26ab684a8b50996a3c80eec6ae4afcb3b32c4dd4
Daniel Cisneros Medina <webmasterofpuppetz@gmail.com>
2021-01-04 19:59:20 -0600

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  #this is how you override the default resource value, also, you can add your own custom route method
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
