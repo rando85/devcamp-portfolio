e5e769611aa81902ebbb1cf16a2797219c9096fe
Daniel Cisneros Medina <webmasterofpuppetz@gmail.com>
2021-01-08 18:39:21 -0600

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   #this is how you override the default resource value, also, you can add your own custom route method
